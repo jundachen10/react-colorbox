b5453f2962d315faf88ca3d2fc828e0261c85eb6
jundachen10 <jundachen10@gmail.com>
2023-06-13 16:46:12 -0400

    figured out how to handle boxes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+import './App.css';
+
+function Square( {onSquareClick, backgroundColor }) {
+  return (
+    <div className="square" onMouseOver={onSquareClick} style={{backgroundColor}} >
+    </div>
+  );
 }
 
+function randomColor() {
+  return "#" + Math.random().toString(16).slice(2,8);
+} //#58FEGD - color
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(24).fill(null));
+
+  function asdf(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+
+  }
+
   return (
     <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+      <div className="Board">
+        <Square value={squares[0]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[1]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[2]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[3]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[4]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[5]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[6]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[7]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[8]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[9]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
       </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
+
     </>
   );
 }
