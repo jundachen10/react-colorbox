3f3dde29971e60d9e0cff61c2ff563520c5da29f
jundachen10 <jundachen10@gmail.com>
2023-06-13 16:53:43 -0400

    removed height from board css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function randomColor() {
 } //#58FEGD - color
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(24).fill(null));
+  const [squares, setSquares] = useState(Array(28).fill(null));
 
   function asdf(i) {
     const nextSquares = squares.slice();
@@ -35,8 +35,27 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
         <Square value={squares[8]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
         <Square value={squares[9]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[10]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[11]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[12]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[13]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[14]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[15]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[16]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[17]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[18]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[19]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[20]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[21]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[22]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[23]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[24]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[25]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[26]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[27]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[28]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
+        <Square value={squares[29]} onSquareClick={() => asdf(0)} backgroundColor={randomColor()} />
       </div>
-
     </>
   );
 }
